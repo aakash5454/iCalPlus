21e2c5e83e3f91b29277b453ddb80fcfd7db963d
skypirate <psal_dun@yahoo.com>
2015-03-27 10:57:09 -0500

    1.0
    1) Implemented UI without constraints
    2) Implemented MainLabel stack

---
+++ New Calculator/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  New Calculator
+//
+//  Created by Sky on 3/26/15.
+//  Copyright (c) 2015 com.sky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
+
